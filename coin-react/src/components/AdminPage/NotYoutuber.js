7e6f6f4274c6fc077d8801f33ae19a3b08549f22
rkdnd <rkdnddl23@gmail.com>
2022-04-21 23:49:37 +0900

    add Youtube API connection feature

--- coin-react/src/components/AdminPage/NotYoutuber.js
+++ coin-react/src/components/AdminPage/NotYoutuber.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header2 from '../Header2';
+import config from '../../config';
 
 const Text1 = styled.input.attrs(props => ({
     type: "text",
@@ -64,7 +66,86 @@ const CustomButton = styled.button`
     border: 2px  #E0E0E0;
     display: block;
 `
+
 function NotYoutuber(){
+    let youtubeUrl = React.createRef();
+
+    // const AddCreator = async (url) => {
+    //     let res = null;
+    //     const words = url.split('/');
+    //     const {api_key} = config;
+    //     var username = words[words.length-1];
+    //     var channelId = null;
+    //     var channelName = null;
+    //     var channelImage = null;
+    //     const searchCall = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${username}&key=${api_key}`;
+    //     await fetch(searchCall)
+    //     .then(result => result.json())
+    //     .then(data => {
+    //         channelId = data.items[0].id.channelId;
+    //         channelName = data.items[0].snippet.title;
+    //         channelImage = data.items[0].snippet.thumbnails.default.url
+    //         });
+    //     // Creator에 저장
+    //     res = await addCreator(userId, "", url, channelId, 1, channelImage, channelName);
+    //     console.log('addCreator finish', res);
+    // }
+
+    async function addCreator2(stringUrl){
+        stringUrl = stringUrl.split('/')
+        const username = stringUrl[stringUrl.length -1]
+        const {api_key} = config;
+        const proxyserver = 'https://cors-anywhere.herokuapp.com/'
+        const searchCall = `https://www.googleapis.com/youtube/v3/channels?id=${username}&key=${api_key}&part=snippet,statistics,brandingSettings`;
+
+        const creatorInfo = {};
+
+        console.log(username);
+        await axios.get(proxyserver+searchCall).then(response => {
+            const data = response.data.items[0];
+            creatorInfo.channelId = username;
+            creatorInfo.channelName = data.snippet.title;
+            creatorInfo.channelthumbnail = data.snippet.thumbnails.high.url;
+            creatorInfo.channeldescription = data.snippet.description;
+            creatorInfo.channelBanner = data.brandingSettings.image.bannerExternalUrl;
+            creatorInfo.channelSubscriberCount = data.statistics.subscriberCount;
+        });
+    }
+
+    function youtuberAuthHandler(){
+        const stringUrl = youtubeUrl.current.value;
+        const user = JSON.parse(localStorage.getItem('user'));
+        
+        if(stringUrl.size === 0){
+            // TODO : URL검증
+        }
+        else{
+            // TODO : 신청 정보 admin 페이지로 보내기
+            
+            addCreator2(stringUrl);
+        }
+    }
+
+
+    // const setCreatorInfo = async (creator) => {
+    //     // 유튜버 정보 셋팅
+    //     console.log('유튜버 정보 셋팅');
+    //     var channelId = creator[0].channel_id;
+    //     const {api_key} = config;
+    //     const infoCall = `https://www.googleapis.com/youtube/v3/channels?part=brandingSettings,snippet,statistics&`;
+    //     await fetch(`${infoCall}id=${channelId}&key=${api_key}`)
+    //     .then(result => result.json())
+    //     .then(data => {
+    //         setSubscriberCount(data.items[0].statistics.subscriberCount);
+    //         if (data.items[0].brandingSettings.image != undefined) {
+    //             setChannelBanner(data.items[0].brandingSettings.image.bannerExternalUrl);
+    //         }
+    //         setChannelNameThumbnails(data.items[0].snippet.thumbnails.default.url);
+    //     });
+    // }
+
+
+
     const Apply = () => (
        <div>
            <Row1>
@@ -75,7 +156,7 @@ function NotYoutuber(){
            </Row3>
        </div>
     )
-    let youtubeUrl = React.createRef();
+
     const NoApply = () => (
        <div>
            <Row1>
@@ -86,7 +167,7 @@ function NotYoutuber(){
                <Font2 style={{"marginTop": "-5px"}}>회원가입 정보와 유튜브 계정이 다를 경우 거절될 수 있습니다.</Font2>
            </Row>
            <Row3>
-               <CustomButton type="button">유튜버 인증 신청</CustomButton>
+               <CustomButton type="button" onClick={youtuberAuthHandler}>유튜버 인증 신청</CustomButton>
            </Row3>
        </div>
     )
--- coin-react/src/config.js
+++ coin-react/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_key: "AIzaSyBZFOq0pZ4L2Yng98fDqKVcOE_ZTxuVhz4", // YoutubeApi key
+    api_key: "AIzaSyAaQ_YSIimzmuB91a_Mj8ltXulsHNc-bR0", // YoutubeApi key
     mintSubscriber: 200, // 기준 구독자
     commission: 100, // 수수료
     publishCount: 101 // 발행뱃지
